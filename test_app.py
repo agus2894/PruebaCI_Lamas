35368b8848cc4574cf3656dfa88cac3bd72554fc
Gonzalo <agustinlamas2011@gmail.com>
2025-10-17 16:14:59 -0300

    Otras Pruebas

--- test_app.py
+++ test_app.py
@@ -2,5 +2,9 @@ from app import suma
 
 def test_suma():
     assert suma(1, 2) == 3
+    assert suma(-1, 1) == 0
+    assert suma(0, 0) == 0
+    assert suma(-1, -1) == -2
+    assert suma(2.5, 2.5) == 5.0
 
     
